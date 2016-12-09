4fba51ef90f303fdb59a31a525491df26376b9c8
natron19 <nathan19@hotmail.com>
2016-12-08 20:27:47 -0800

    initial commit

---
+++ app/models/product.rb
@@ -0,0 +1,12 @@
+class Product < ActiveRecord::Base
+  belongs_to :user
+  has_attached_file :file
+  has_many :sales
+
+  validates_numericality_of :price, greater_than: 0.49, message: "must be at least 50 cents"
+
+  validates_attachment_content_type :file, :content_type => [
+    "image/jpg", "image/jpeg", "image/png", "image/gif", "application/pdf", "application/zip"
+  ]
+
+end
